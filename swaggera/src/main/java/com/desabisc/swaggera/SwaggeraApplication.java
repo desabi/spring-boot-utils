7c1aa6a3cf4df055bf185e7633b40dd9527bb074
abi <desabi@live.com>
2023-11-28 20:36:35 -0600

    feat: add swaggera project

---
+++ swaggera/src/main/java/com/desabisc/swaggera/SwaggeraApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.swaggera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggeraApplication.class, args);
+	}
+
+}
---
+++ swaggera/src/test/java/com/desabisc/swaggera/SwaggeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabisc.swaggera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
