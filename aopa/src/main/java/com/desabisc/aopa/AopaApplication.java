bfa21fb608444365386710578e8e85454d1b28ba
abigahil.delgado <abigahil.delgado@ine.mx>
2023-12-01 12:14:39 -0600

    feat: add aopa project

---
+++ aopa/src/main/java/com/desabisc/aopa/AopaApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.aopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopaApplication.class, args);
+	}
+
+}
