72a289ddac1c567290e7e1dbc6e6106534ea02fd
desabi <desabi@live.com>
2026-03-28 12:38:59 -0600

    feat: api versioning, header example, base project

---
+++ api-versioning-header/src/main/java/com/desabi/guide/api/versioning/header/ApiVersioningHeaderApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.api.versioning.header;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVersioningHeaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVersioningHeaderApplication.class, args);
+	}
+
+}
---
+++ api-versioning-header/src/test/java/com/desabi/guide/api/versioning/header/ApiVersioningHeaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.api.versioning.header;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVersioningHeaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
